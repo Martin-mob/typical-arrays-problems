4c07052bd988d008a75c23c40b7f16d41c47a737
Martin-mob <yulia.mart95@gmail.com>
2021-03-10 23:11:53 +0300

    fix: fixed a bug in the code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 
 exports.min = function min (array) {
-  if (array == 0 && array == ' ') {
+  if (array === undefined || array.length == 0) {
     return 0;
-  } esle {
-    return Math.min(array);
+  } else {
+    return Math.min.apply(null, array);
   }
 }
 
 exports.max = function max (array) {
-  if (array == 0 && array == ' ') {
+  if (array === undefined || array.length == 0) {
     return 0;
-  } esle {
-    return Math.max(array);
+  } else {
+    return Math.max.apply(null, array);
   }
 }
 
 exports.avg = function avg (array) {
-  if (array == 0 && array == ' ') {
+  if (array === undefined || array.length == 0) {
     return 0;
-  } esle {
-    return array.reduce((sum, current) => (sum + current)/array.lenght, 0);
+  } else {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]; 
+    }
+    return (result / array.length);
   }
 }
